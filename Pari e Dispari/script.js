459854f824146efb66c8334cd65cc4db1983cf46
RobertoFloris <florisroberto99@gmail.com>
2024-11-19 17:50:06 +0100

    [fix] description and logic in script PARI E DISPARI

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -3,33 +3,58 @@
 
  * Creo una funzione in grado di generare un numero random da 1 a 5
 
- * Creo una funzione in grado di stabilire, dati due parametri numerici, se la loro somma è pari o dispari
+ * Creo una funzione in grado di stabilire se un numero è pari o dispari
 
  * Salvo in una variabile la scelta random del pc, richiamando la funzione capace di generarlo
 
- * Richiamo la funzione in grado di stabilire se la loro somma è pari o dispari
+ * Sommo le due scelte
+
+ * Verifico che la somma sia pari o dispari, con la relativa funzione
 
  * Se la scelta dell'utente coincide con l'esito della funzione, lo dichiaro vincitore 
  */
 
+// const pariDispari = prompt("Inserisci la tua scelta. Pari o dispari?");
+
+// const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
 const pariDispari = "pari";
-// const pariDispari = "dispari"; 
+
 const numeroUtente = 5;
+
 const numeroPc = randomNumber();
 
-console.log(numeroUtente);
-console.log(numeroPc);
-console.log(numeroPc + numeroUtente);
-console.log(isPari(numeroUtente, numeroPc));
+const somma = numeroUtente + numeroPc;
 
+sommaIsPari = isPari(somma);
+
+console.log(pariDispari, numeroUtente, numeroPc, somma, sommaIsPari)
+
+if(pariDispari === "pari"){
+
+  if(isPari(somma)){
+    message = "hai vinto!"
+  } else {
+    message = "hai perso!"
+  }
+
+} else {
+  if(!isPari(somma)){
+    message = "hai vinto!"
+  } else {
+    message = "hai perso!"
+  }
+}
+
+console.log(message);
 
 
 function randomNumber(){
   return Math.floor(Math.random() * 5) + 1;
 }
 
-function isPari(num1, num2){
-  if( !((num1+num2) % 2) ){
+function isPari(num){
+  if( !(num % 2) ){
     return true;
   } else {
     return false;
