81cd6d6644c3ef2938e3d697427546989f53780c
RobertoFloris <florisroberto99@gmail.com>
2024-11-19 16:28:54 +0100

    [add] job description in script Palindroma

---
+++ Palindroma/script.js
@@ -0,0 +1,15 @@
+/**
+ * Chiedo una parola all'utente
+
+ * La parola passerà come parametro stringa alla mia funzione isPalindroma
+
+ * Creo un array composto dalle lettere della parola inserita e le inverto
+
+ * Unisco l'array di lettere in una sola variabile parolaInvertita  
+
+ * Paragono la parola iniziale con parolaInvertita
+
+ * Se sono uguali restituirà true, altrimenti false
+
+ */
+
