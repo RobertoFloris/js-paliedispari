40f23f53407bc2557495ba99f1f89da484e3d6ee
RobertoFloris <florisroberto99@gmail.com>
2024-11-19 16:51:11 +0100

    [add] job description in script Pari e Dispari

---
+++ Pari e Dispari/script.js
@@ -0,0 +1,13 @@
+/**
+ * Salvo in due variabili le scelte dell'utente
+
+ * Creo una funzione in grado di generare un numero random da 1 a 5
+
+ * Creo una funzione in grado di stabilire, dati due parametri numerici, se la loro somma è pari o dispari
+
+ * Salvo in una variabile la scelta random del pc, richiamando la funzione capace di generarlo
+
+ * Richiamo la funzione in grado di stabilire se la loro somma è pari o dispari
+
+ * Se la scelta dell'utente coincide con l'esito della funzione, lo dichiaro vincitore 
+ */
